2385930eb15b11c09a6b661ad4ad5ff23dd54d0a
Mihail-K <Mihail.K@fimu.tk>
2016-02-06 20:30:15 -0500

    Add missing package files.

---
+++ source/markov/json/package.d
@@ -0,0 +1,5 @@
+
+module markov.json;
+
+public import markov.json.decoder;
+public import markov.json.encoder;
--- source/markov/package.d
+++ source/markov/package.d
@@ -3,4 +3,6 @@ module markov;
 
 public import markov.chain;
 public import markov.counter;
+public import markov.json;
+public import markov.serialize;
 public import markov.state;
